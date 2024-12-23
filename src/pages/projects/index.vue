408d60fdb036081222f5caea3c456ae9cc5c6293
Kaipo Kent <kaipo@adhocteam.us>
2024-12-23 16:00:25 -0700

    Cache use projects

--- src/pages/projects/index.vue
+++ src/pages/projects/index.vue
@@ -1,18 +1,12 @@
 <script setup lang="ts">
 import { columns } from '@/utils/tableColumns/projectsColumns'
-import { projectsQuery } from '@/utils/supaQueries'
-import type { Projects } from '@/utils/supaQueries'
+import { useProjectsStore } from '@/stores/loaders/projects'
 
 usePageStore().pageData.title = 'Projects'
 
-const projects = ref<Projects | null>(null)
-const getProjects = async () => {
-  const { data, error, status } = await projectsQuery
-
-  if (error) useErrorStore().setError({ error, customCode: status })
-
-  projects.value = data
-}
+const projectsLoader = useProjectsStore()
+const { projects } = storeToRefs(projectsLoader)
+const { getProjects } = projectsLoader
 
 await getProjects()
 </script>
